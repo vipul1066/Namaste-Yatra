deb801bf86eaaba4a071a913d62c5d406ce2acb0
vipul1066 <vipulkatale10@gmail.com>
2024-07-28 22:03:02 +0530

    Add Project Files

---
+++ init/index.js
@@ -0,0 +1,26 @@
+const mongoose = require("mongoose");
+const initData = require("./data.js");
+const Listing = require("../models/listing.js");
+
+const MONGO_URL = "mongodb://127.0.0.1:27017/homeswap";
+
+main()
+  .then(() => {
+    console.log("connected to DB");
+  })
+  .catch((err) => {
+    console.log(err);
+  });
+
+async function main() {
+  await mongoose.connect(MONGO_URL);
+}
+
+const initDB = async () => {
+  await Listing.deleteMany({});
+  initData.data = initData.data.map((obj)=> ({ ...obj, owner: "65cd9697abaa0fa5d8b557cd"}) );
+  await Listing.insertMany(initData.data);
+  console.log("data was initialized");
+};
+
+initDB();
